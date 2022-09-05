038677717538eb3b4d3dff2c1c7a1f603692c528
Tomer Avraham <avrahamtomer710@gmail.com>
2022-09-06 00:44:05 +0300

    change to hashRouter

--- src/index.js
+++ src/index.js
@@ -3,11 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
-<<<<<<< HEAD
 import { HashRouter } from "react-router-dom"
-=======
-import { HashRouter } from "react-router-dom";
->>>>>>> 7a1f7f2311f6770d3a3768e34cf1a221e0dac82b
 import { ColorModeContextProvider } from "./context/ColorModeContext"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
