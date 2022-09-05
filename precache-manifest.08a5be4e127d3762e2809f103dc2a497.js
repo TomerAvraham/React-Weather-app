da0deb86641ca0eb374cc5f1cd310758da77bbbe
Tomer Avraham <avrahamtomer710@gmail.com>
2022-09-05 23:13:00 +0300

    Updates

---
+++ precache-manifest.08a5be4e127d3762e2809f103dc2a497.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c3f9e76cc3d92b209b3f",
+    "url": "/Tomer-Avraham-31-08-2022/static/css/main.88d119fa.chunk.css"
+  },
+  {
+    "revision": "c3f9e76cc3d92b209b3f",
+    "url": "/Tomer-Avraham-31-08-2022/static/js/main.c3f9e76c.chunk.js"
+  },
+  {
+    "revision": "98e8c77ab904c431269a",
+    "url": "/Tomer-Avraham-31-08-2022/static/js/1.98e8c77a.chunk.js"
+  },
+  {
+    "revision": "064130c852d12e6d5d60",
+    "url": "/Tomer-Avraham-31-08-2022/static/js/runtime~main.064130c8.js"
+  },
+  {
+    "revision": "86e015404d77ffc36edb6e9b896c2349",
+    "url": "/Tomer-Avraham-31-08-2022/static/media/abra-navbar-logo.86e01540.png"
+  },
+  {
+    "revision": "44319328dbd8acd7a2e408942ec9fcb7",
+    "url": "/Tomer-Avraham-31-08-2022/static/media/currentForecastBackground.44319328.png"
+  },
+  {
+    "revision": "f9960ff92e8b3d35b88b10208ec87a4d",
+    "url": "/Tomer-Avraham-31-08-2022/index.html"
+  }
+];
